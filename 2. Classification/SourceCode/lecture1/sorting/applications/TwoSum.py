d916e612ed21a2b15257c19f7be967531489285f
Xinrui <64628076+xinrui112@users.noreply.github.com>
2021-01-11 12:16:55 -0600

    Create TwoSum.py

---
+++ 2. Classification/SourceCode/lecture1/sorting/applications/TwoSum.py
@@ -0,0 +1,9 @@
+class TwoSum(object):
+    def twoSum(self, nums, target):
+        dic = {}
+        for i, num in enumerate(nums):
+            if num in dic:
+                return [dic[num], i]
+            else:
+                dic[target - num] = i
+                
